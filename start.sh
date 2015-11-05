bffadffb9fa9f4c2b62f0ea77760a0b63fcc4bcc
Locksfree <locksfree@gmail.com>
2015-11-05 12:00:07 +0000

    Remove error redirect

--- start.sh
+++ start.sh
@@ -6,33 +6,35 @@ if [ $UID != 0 ]; then
 fi
 
 echo "Updating..."
-apt-get update > /dev/null 2>&1
-echo "Installing ack..." 2>&1
-apt-get install -yf ack-grep > /dev/null 2>&1
-echo "Installing ctags..." 2>&1
-apt-get install -yf exuberant-ctags > /dev/null 2>&1
+apt-get update > /dev/null 
+echo "Installing ack..." 
+apt-get install -yf ack-grep > /dev/null 
+echo "Installing ctags..." 
+apt-get install -yf exuberant-ctags > /dev/null 
 echo "Installing fontconfig..."
 apt-get install -yf fontconfig > /dev/null
 echo "Installing git..."
-apt-get install -yf git > /dev/null 2>&1
+apt-get install -yf git > /dev/null 
 echo "Installing mercurial..."
-apt-get install -yf mercurial > /dev/null 2>&1
+apt-get install -yf mercurial > /dev/null 
 echo "Installing xfce4..."
-apt-get install -yf xfce4 > /dev/null 2>&1
+apt-get install -yf xfce4 > /dev/null 
 echo "Installing terminator..."
-apt-get install -yf terminator > /dev/null 2>&1
+apt-get install -yf terminator > /dev/null 
 echo "Installing vim..."
-apt-get install -yf vim > /dev/null 2>&1
+apt-get install -yf vim > /dev/null 
+echo "Installing zsh..."
+apt-get install -yf zsh > /dev/null
 
 if [ `vim --version | ack '([+]ruby)' | wc -l` -eq 0 ]
 then
    echo "Missing ruby support in vim. Recompiling..."
    echo "Removing installed package..."
-   apt-get remove -yf vim-common vim-runtime > /dev/null 2>&1
+   apt-get remove -yf vim-common vim-runtime > /dev/null 
    echo "Installing build dependencies..."
-   apt-get build-dep -yf vim > /dev/null 2>&1
+   apt-get build-dep -yf vim > /dev/null 
    echo "Cloning..."
-   hg clone https://vim.googlecode.com/hg/ /tmp/vim > /dev/null 2>&1
+   hg clone https://vim.googlecode.com/hg/ /tmp/vim > /dev/null 
    cd /tmp/vim
    echo "Configuring..."
    ./configure --enable-pythoninterp --enable-rubyinterp
